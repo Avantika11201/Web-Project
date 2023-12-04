9ae7170a8b0a2fb7f2465caf14596915c66eb1f0
Avantika11201 <avantikajewani.bscsf21@iba-suk.edu.pk>
2023-12-04 12:47:02 +0500

    initial commit web project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import './App.css';
-import { Header } from './Components/Header';
-import { Footer } from './Components/Footer';
-import { HeroSection } from './Components/HeroSection';
-import {Routes, Route} from 'react-router-dom';
-import { About } from './Components/About';
+import React from 'react';
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Routes>
-        <Route path="/HeroSection" element={<HeroSection/>}/>
-        <Route path="/About" element={<About/>} />
-      </Routes>
-      <Footer/>
+      
     </div>
   );
 }
