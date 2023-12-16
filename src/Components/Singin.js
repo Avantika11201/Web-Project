6c1dc857eee7928bff32447b66b7fd3385c998e7
Abdullah Memon <abdullahmemon.bscsf21@iba-suk.edu.pk>
2023-12-16 18:18:14 +0500

    set up of fonts and signin page completed

--- src/Components/Singin.js
+++ src/Components/Singin.js
@@ -2,6 +2,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../assets/css/signin.css';
+
 const Signin = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -12,20 +14,18 @@ const Signin = () => {
   };
 
   return (
-    <div>
+    <div className='signin_form'>
       <h2>Signin</h2>
-      <label>
-        Email:
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </label>
+      
+      <input type="email" value={email} placeholder='Email Address' onChange={(e) => setEmail(e.target.value)} />
+    
+      <input type="password" value={password} placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
+      
       <button onClick={handleSignin}>Signin</button>
       <p>
         Don't have an account? <Link to="/signup">Signup here</Link>
       </p>
+    
     </div>
   );
 };
